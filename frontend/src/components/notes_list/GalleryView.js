b0413a0b2d002f50a452ea423bf1004fef675808
ergi98 <edervishaj98@gmail.com>
2020-10-19 13:39:19 +0200

    Fixed some issues on grid view

--- frontend/src/components/notes_list/GalleryView.js
+++ frontend/src/components/notes_list/GalleryView.js
@@ -15,8 +15,8 @@ function GalleryView(props) {
                     <Grow key={note.id} in={true} style={{ transformOrigin: '0 0 0' }} timeout = {250 + index*150}>
                         <div className="gallery-note">
                             <div className="gallery-preview" onClick={() => props.setOpenedNote(note)}>
-                                <p className="preview-paragraph" dangerouslySetInnerHTML={{__html: note.text}}>
-                                </p>
+                                <div className="preview-paragraph" dangerouslySetInnerHTML={{__html: note.text}}>
+                                </div>
                                 <div className={`grid-note-info ${note.id === props.openendNote?.id? "opened-note" : ""}`}>
                                     <h4 className={`grid-title ${note.id === props.openendNote?.id? "white" : ""}`}>{note.title}</h4>
                                     <label className={`grid-time ${note.id === props.openendNote?.id? "white" : ""}`}><DisplayDate date={note.updated_at}/></label>
